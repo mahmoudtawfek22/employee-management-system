3a090b3ca40c22e8d06cd061aeb96ea2353d6aba
mahmoudtawfek22 <momohoho2224@gmail.com>
2024-07-10 14:01:00 +0300

    creating footer and modify responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterModule, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
